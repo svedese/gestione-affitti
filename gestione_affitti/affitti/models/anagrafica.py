246ba4dbcc19046548c761e715a7b1d2d464aea5
svedese <37276705+svedese@users.noreply.github.com>
2018-04-16 14:07:56 +0200

    Update anagrafica.py

--- gestione_affitti/affitti/models/anagrafica.py
+++ gestione_affitti/affitti/models/anagrafica.py
@@ -17,7 +17,7 @@ class PersonaFisica(models.Model):
     nascita_data = models.DateField(verbose_name='data di nascita', help_text='')
     nascita_luogo = models.CharField(max_length=100, verbose_name='luogo di nascita', help_text='')
     codice_fiscale = models.CharField(max_length=16, help_text='max_length=16')
-    partita_iva = models.CharField(max_length=20, help_text='')
+    # partita_iva = models.CharField(max_length=20, help_text='')
     indirizzo = models.ForeignKey(Indirizzo, on_delete=models.SET_NULL, null=True, help_text='')
     recapito = models.ForeignKey(Recapito, on_delete=models.SET_NULL, null=True, help_text='')
 
@@ -25,7 +25,7 @@ class PersonaFisica(models.Model):
         return '%s %s' % (self.cognome, self.nome)
 
     class Meta:
-        abstract = True
+        # abstract = True
         verbose_name = 'persona fisica'
         verbose_name_plural = 'persone fisiche'
 
@@ -34,7 +34,7 @@ class PersonaGiuridica(models.Model):
     nome = models.CharField(max_length=100, primary_key=True, help_text='')
     tipo = models.CharField(max_length=100, help_text='')
     codice_fiscale = models.CharField(max_length=16, help_text='max_length=16')
-    partita_iva = models.CharField(max_length=20, blank=True)
+    partita_iva = models.CharField(max_length=11, blank=True)
     ufficio = models.CharField(max_length=100, blank=True)
     rea = models.CharField(max_length=100, blank=True)
     costituzione_anno = models.CharField(max_length=4, blank=True)
@@ -47,6 +47,6 @@ class PersonaGiuridica(models.Model):
         return self.nome
 
     class Meta:
-        abstract = True
+        # abstract = True
         verbose_name = 'persona giuridica'
         verbose_name_plural = 'persone giuridiche'
